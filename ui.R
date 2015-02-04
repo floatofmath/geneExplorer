d0846e97f46b2000539e14ed720fde4c31078aed
Florian Klinglmueller <float@lefant.net>
2015-02-04 16:02:52 +0100

    have started to modularize the app, so it can be easily ported to other projects

--- ui.R
+++ ui.R
@@ -25,57 +25,9 @@ shinyUI(fluidPage(
     ),
     mainPanel(
         tabsetPanel(
-            tabPanel(title="Heatmap",
-                     conditionalPanel(
-                         condition="input.sort == 'top'",
-                         sliderInput("toptags",
-                                     label="Number of top tags to show",
-                                     min=10,max=500,step=10,value=100)
-                     ),
-                     plotOutput("heatmap",height='1080px')),
-            tabPanel("Correlation",
-                     fluidRow(
-                         column(4,
-                                selectInput("gsort",
-                                            label="Select genes to correlate",
-                                            choices=c("top","all","custom"),
-                                            selected="top"),
-                                conditionalPanel(
-                                    condition="input.gsort == 'top'",
-                                    sliderInput("ngenes",
-                                                label="Number of top tags to show",
-                                                min=10,max=1000,step=10,value=100)),
-                                textOutput("corsize")),
-                         column(4,
-                                selectInput("lsort",
-                                            label="Select loci to correlate",
-                                            choices=c("top","all"),
-                                            selected="top"),
-                                conditionalPanel(
-                                    condition="input.lsort == 'top'",
-                                    sliderInput("nloci",
-                                                label="Number of top tags to show",
-                                                min=10,max=1000,step=10,value=100)),
-                                actionButton("compute","Apply and recompute correlation matrix")),
-                         column(4,
-                                sliderInput("cutoff",
-                                            label="Cutoff above which an edge is drawn",
-                                            min=0,
-                                            max=1,
-                                            value=0.7,step=0.01),
-                                textOutput("graphsize"),
-                                actionButton("plot","Apply cutoff and plot graph"))
-                     ),
-                     plotOutput("graph",height="1080px")),
-            tabPanel("Table",
-                     conditionalPanel(
-                         condition="input.method != 'c'",
-                         sliderInput("tabtags",
-                                     label="Number of top tags to show",
-                                     min=100,max=1500,step=200,value=100)
-                     ),
-                     tableOutput("table")
-                     )
+            heatmap_output(),
+            correlation_graph(),
+            gene_table()
         )
     )
     )
